b4a8aa735a128bc887b7d33541c6619353163922
josh <joshua.97@icloud.com>
2026-01-07 12:30:56 +0000

    Fixed references

--- 3.2P/public/js/scripts.js
+++ 3.2P/public/js/scripts.js
@@ -3,8 +3,54 @@ const clickMe = () => {
 }
 
 $(document).ready(function () {
-    $('materialboxed').materialbox();
+    $('.materialboxed').materialbox();
     $("#clickMeButton").click(() => {
         clickMe();
     })
+})
+
+const cardList = [
+    {
+        title: "Panigale V4",
+        link: "https://www.ducati.com/au/en/bikes/panigale",
+        imagelink: "images/panigale.jpg",
+        moreinfo: "The Panigale V4 is a masterpiece of engineering and design, combining cutting-edge technology with Italian craftsmanship to deliver an unparalleled riding experience."
+    },
+    {
+        title: "DesertX",
+        link: "https://www.ducati.com/au/en/bikes/desertx",
+        imagelink: "images/desertx.jpg",
+        moreinfo: "The DesertX is built to conquer the toughest terrains, featuring a robust frame, advanced suspension, and a powerful engine that ensures optimal performance both on and off-road."
+    },
+    {
+        title: "Scrambler",
+        link: "https://www.ducati.com/au/en/bikes/scrambler",
+        imagelink: "images/scrambler.jpg",
+        moreinfo: "The Scrambler combines retro style with modern performance, offering a versatile and fun riding experience that appeals to both new and experienced riders alike."
+    }
+]
+
+const addCards = (items) => {
+    items.forEach(item => {
+        const card = `<div class="col s12 m4">
+        <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="${item.imagelink}">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">${item.title}<i class="material-icons right">more_vert</i></span>
+                <p><a href="${item.link}">Buy now</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">${item.title}<i class="material-icons right">close</i></span>
+                <p>${item.moreinfo}</p>
+            </div>
+        </div>
+    </div>`;
+        $("#card-section").append(card);
+    });
+}
+
+$(document).ready(function () {
+    addCards(cardList);
 })
